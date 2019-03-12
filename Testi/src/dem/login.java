2a17ddade0949fb02a83901473064e10edfa61a2
Ajit <Ajit@192.168.2.2>
2019-03-12 23:21:29 +0530

    helllo

--- Testi/src/dem/login.java
+++ Testi/src/dem/login.java
@@ -1,5 +1,13 @@
 package dem;
 
-public class login {
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
+public class login
+{
+	@Test
+public void test()
+{
+	Reporter.log("Ajit",true);
+}
 }
